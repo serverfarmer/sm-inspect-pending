f0a917253b9c04ced8397603febeb3a89a399b50
Tomasz Klim <github@tomaszklim.pl>
2020-03-24 16:37:38 +0100

    renaming sf-farm-manager extension

--- cron/inspect.sh
+++ cron/inspect.sh
@@ -6,8 +6,8 @@ command="/opt/farm/ext/packages/utils/pending.sh"
 
 for server in `/opt/farm/mgr/inspect-pending/utils/get-hosts.sh`; do
 
-	host=`/opt/farm/ext/farm-manager/internal/decode.sh host $server`
-	port=`/opt/farm/ext/farm-manager/internal/decode.sh port $server`
+	host=`/opt/farm/mgr/farm-manager/internal/decode.sh host $server`
+	port=`/opt/farm/mgr/farm-manager/internal/decode.sh port $server`
 
 	sshkey=`/opt/farm/ext/keys/get-ssh-management-key.sh $host`
 	ssh -i $sshkey -p $port -o StrictHostKeyChecking=no root@$host "$command" \
